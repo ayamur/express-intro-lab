f3ec9af9f28be626f8d83041ccb6bb40658168bc
AyaMu <ayalamuruch@gmail.com>
2023-01-03 18:50:13 -0500

    add imports for express and merTypes, add express app, add app configure, add mount routes for /, /home, and /mermaids, and app listen to port number

--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 //import modules
 
+import express from 'express'
+import { merTypes } from './data/intlmermaids-data.js'
+
 //create express app
 
+const app = express()
+
 //configure the app - app.set(__ , __)
+
+app.set('view engine', 'ejs')
 
 //mount middleware - app.use
 
 //mount routes
 
+app.get('./', function(req, res) {
+  res.redirect('/home')
+})
+
+app.get('./mermaids', function(req, res) {
+  res.render('mermaids/index', {
+    myMerList: merTypes
+  })
+})
+
 //tell app listen on port 3000
+
+app.listen(3000, function() {
+  console.log('I am listening to port 3000!')
+})
