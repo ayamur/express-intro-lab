3d77044badddaf55749c2ad711dd4e55772ea990
AyaMu <ayalamuruch@gmail.com>
2023-01-03 18:40:54 -0500

    create merTypes array and add export for merTypes

---
+++ data/intlmermaids-data.js
@@ -0,0 +1,15 @@
+const merTypes = [
+  {text: 'Ningyo', text: 'from: Japanese lore', known: false, _id: 001},
+  {text: 'Selkie', text: 'from: Gaelic stories', known: true, _id: 002},
+  {text: 'Iara', text: 'from: Brazil and the Amazon', researched: false, _id: 003},
+  {text: 'Mami Wata', text: 'from: African legends', known: false, _id: 004},
+  {text: 'Marakihau', text: 'from: New Zealand and Maori spoken tales and pictures', known: false, _id: 005},
+  {text: 'Melusine', text: 'from: French and other European medieval folk stories', known: true, _id: 006},
+  {text: 'Merrow', text: 'from: Ireland', known: true, _id: 007},
+  {text: 'Rusalka', text: 'from: Russian and Slavic mythology', known: false, _id: 008},
+  {text: 'Finfolk', text: 'from: Normway and Orkney Islands', known: false, _id: 009},
+]
+
+export {
+  merTypes
+}
